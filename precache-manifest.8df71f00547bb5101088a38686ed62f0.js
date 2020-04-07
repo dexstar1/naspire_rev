e81ba1c1144ad95ee98f12e4d3e0256cc03c0c46
dexstar1 <jonsaix852@gmail.com>
2020-04-07 01:11:10 +0100

    Updates

---
+++ precache-manifest.8df71f00547bb5101088a38686ed62f0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f0462aec6bcafd991e6e5792f9f596",
+    "url": "/naspire_rev/index.html"
+  },
+  {
+    "revision": "1a6c984da07ccd6f4a5f",
+    "url": "/naspire_rev/static/css/main.797f5efd.chunk.css"
+  },
+  {
+    "revision": "f2aff46a2a9978189deb",
+    "url": "/naspire_rev/static/js/2.e2f28025.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/naspire_rev/static/js/2.e2f28025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6c984da07ccd6f4a5f",
+    "url": "/naspire_rev/static/js/main.b6d98a96.chunk.js"
+  },
+  {
+    "revision": "833d69bb3b8f2b58e3e0",
+    "url": "/naspire_rev/static/js/runtime-main.89716002.js"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/naspire_rev/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "4b0afb4a9531dbef4d55fa94387fbb4b",
+    "url": "/naspire_rev/static/media/Prata-Regular.4b0afb4a.ttf"
+  },
+  {
+    "revision": "94f51986ecc01449d8a2e7770e8d9095",
+    "url": "/naspire_rev/static/media/image1.94f51986.jpg"
+  },
+  {
+    "revision": "b4fcf3430a5625875718d111eea6d183",
+    "url": "/naspire_rev/static/media/image2.b4fcf343.jpg"
+  },
+  {
+    "revision": "51ef53c32d5c54190e2f15b325790b30",
+    "url": "/naspire_rev/static/media/image3.51ef53c3.jpg"
+  },
+  {
+    "revision": "c07a487d776804c7f978bd3b21559c63",
+    "url": "/naspire_rev/static/media/image4.c07a487d.jpg"
+  },
+  {
+    "revision": "d39568732bcfb55824dc0521d2b1a69b",
+    "url": "/naspire_rev/static/media/image5.d3956873.jpg"
+  },
+  {
+    "revision": "2fb020978e342ab902ebfc964f2c8c4b",
+    "url": "/naspire_rev/static/media/image6.2fb02097.jpg"
+  },
+  {
+    "revision": "41817894b519dcc2dcecbb15751cb1c5",
+    "url": "/naspire_rev/static/media/image7.41817894.jpg"
+  },
+  {
+    "revision": "d033040840442f48963d1671dee2bd96",
+    "url": "/naspire_rev/static/media/image9.d0330408.png"
+  },
+  {
+    "revision": "07cd6396748a50bf13eb9f7b322a05ec",
+    "url": "/naspire_rev/static/media/saas.07cd6396.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/naspire_rev/precache-manifest.8df71f00547bb5101088a38686ed62f0.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/naspire_rev/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
